adcf5e3f068cb1a369aed772a36aa76a5c1c0d8f
kastik <kostas21422112@gmail.com>
2025-11-28 13:42:03 +0200

    [IEE-32] General improvements
    - Moved Usecases, Repos, Datasources associate with getting user info to separate classes
    - Added methods for fetching subscribable tags and subscribing users to new tags

--- core/data/src/main/kotlin/com/kastik/apps/core/data/mappers/UserMappers.kt
+++ core/data/src/main/kotlin/com/kastik/apps/core/data/mappers/UserMappers.kt
@@ -1,14 +1,27 @@
 package com.kastik.apps.core.data.mappers
 
 import com.kastik.apps.core.model.aboard.UserProfile
+import com.kastik.apps.core.model.aboard.UserSubscribableTag
 import com.kastik.apps.core.model.aboard.UserSubscribedTag
 import com.kastik.apps.core.network.model.aboard.UserProfileDto
+import com.kastik.apps.core.network.model.aboard.UserSubscribableTagsDto
 import com.kastik.apps.core.network.model.aboard.UserSubscribedTagDto
 
 fun UserSubscribedTagDto.toDomain() = UserSubscribedTag(
     id = id,
     title = title,
 )
+
+fun UserSubscribableTagsDto.toDomain(): UserSubscribableTag = UserSubscribableTag(
+    id = id,
+    title = title,
+    parentId = parentId,
+    isPublic = isPublic,
+    createdAt = createdAt,
+    updatedAt = updatedAt,
+    deletedAt = deletedAt,
+    mailListName = mailListName,
+    subTags = subTags.map { it.toDomain() })
 
 fun UserProfileDto.toDomain() = UserProfile(
     id = id,
