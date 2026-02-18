1bea4b2315107932f4db0ab24407b0b41239f2c6
kastik <kostas21422112@gmail.com>
2026-02-18 03:34:43 +0200

    [IEE-68] General improvements
    - Move filter refreshing to domain

--- feature/home/src/main/kotlin/com/kastik/apps/feature/home/HomeScreenViewModel.kt
+++ feature/home/src/main/kotlin/com/kastik/apps/feature/home/HomeScreenViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.kastik.apps.core.common.extensions.combine
-import com.kastik.apps.core.domain.service.Notifier
 import com.kastik.apps.core.domain.usecases.AreFabFiltersEnabledUseCase
 import com.kastik.apps.core.domain.usecases.GetFilterOptionsUseCase
 import com.kastik.apps.core.domain.usecases.GetForYouAnnouncementsUseCase
@@ -14,24 +13,12 @@ import com.kastik.apps.core.domain.usecases.GetHomeAnnouncementsUseCase
 import com.kastik.apps.core.domain.usecases.GetIsSignedInUseCase
 import com.kastik.apps.core.domain.usecases.GetQuickResultsUseCase
 import com.kastik.apps.core.domain.usecases.IsForYouEnabledUseCase
-import com.kastik.apps.core.domain.usecases.RefreshAnnouncementTagsUseCase
-import com.kastik.apps.core.domain.usecases.RefreshAuthorsUseCase
 import com.kastik.apps.core.domain.usecases.SetHasSkippedSignInUseCase
 import com.kastik.apps.core.domain.usecases.ShowSignInNoticeRationalUseCase
-import com.kastik.apps.core.model.error.ConnectionError
-import com.kastik.apps.core.model.error.GeneralRefreshError
-import com.kastik.apps.core.model.error.ServerError
-import com.kastik.apps.core.model.error.StorageError
-import com.kastik.apps.core.model.error.TimeoutError
-import com.kastik.apps.core.model.error.UnknownError
-import com.kastik.apps.core.model.result.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -45,10 +32,7 @@ class HomeScreenViewModel @Inject constructor(
     getForYouAnnouncementsUseCase: GetForYouAnnouncementsUseCase,
     getHomeAnnouncementsUseCase: GetHomeAnnouncementsUseCase,
     areFabFiltersEnabledUseCase: AreFabFiltersEnabledUseCase,
-    private val notifier: Notifier,
     private val setHasSkippedSignInUseCase: SetHasSkippedSignInUseCase,
-    private val refreshAuthorsUseCase: RefreshAuthorsUseCase,
-    private val refreshAnnouncementTagsUseCase: RefreshAnnouncementTagsUseCase,
     private val getQuickResultsUseCase: GetQuickResultsUseCase,
 ) : ViewModel() {
 
@@ -75,8 +59,6 @@ class HomeScreenViewModel @Inject constructor(
             enableForYou = enableForYou,
             enableFabFilters = enableFabFilters
         )
-    }.onStart {
-        refreshFilters()
     }.stateIn(
         scope = viewModelScope, started = SharingStarted.Lazily, initialValue = UiState()
     )
@@ -85,30 +67,10 @@ class HomeScreenViewModel @Inject constructor(
 
     val forYouFeedAnnouncements = getForYouAnnouncementsUseCase().cachedIn(viewModelScope)
 
-    private suspend fun refreshFilters() = coroutineScope {
-        val authorsResultDeferred = async { refreshAuthorsUseCase() }
-        val tagsResultDeferred = async { refreshAnnouncementTagsUseCase() }
-
-        listOf(authorsResultDeferred.await(), tagsResultDeferred.await())
-            .filterIsInstance<Result.Error<GeneralRefreshError>>()
-            .firstOrNull()
-            ?.let { refreshError ->
-                notifier.sendToastNotification(refreshError.error.toUserMessage())
-            }
-    }
-
     fun onSignInNoticeDismiss() {
         viewModelScope.launch {
             setHasSkippedSignInUseCase(true)
         }
     }
 
-}
-
-private fun GeneralRefreshError.toUserMessage(): String = when (this) {
-    ConnectionError -> "Network unavailable. Please check your connection."
-    ServerError -> "Server error. Unable to refresh filters."
-    StorageError -> "Storage error. Unable to cache filters."
-    TimeoutError -> "Connection timed out."
-    UnknownError -> "An unexpected error occurred."
 }
