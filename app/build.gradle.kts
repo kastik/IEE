1605a5b3ef656f4e23a8e19d5986a238b5b193d7
kastik <kostas21422112@gmail.com>
2026-02-23 08:54:00 +0200

    [IEE-65] Splash screen
    - Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ configure<ApplicationExtension> {
 
     defaultConfig {
         applicationId = "com.kastik.apps"
-        versionCode = 29
-        versionName = "2.0.1"
+        versionCode = 30
+        versionName = "2.1.0"
     }
 
     val keystoreProperties = Properties().apply {
