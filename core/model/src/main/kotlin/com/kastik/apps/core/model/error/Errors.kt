9cd39f5af9aa47d83064504811a13ef735475fbc
kastik <kostas21422112@gmail.com>
2026-03-04 02:36:24 +0200

    [IEE-72] Remove deprecated api calls
    - Refine Error Classes

--- core/model/src/main/kotlin/com/kastik/apps/core/model/error/Errors.kt
+++ core/model/src/main/kotlin/com/kastik/apps/core/model/error/Errors.kt
@@ -1,12 +1,20 @@
 package com.kastik.apps.core.model.error
 
-sealed interface AuthenticatedRefreshError
+interface AppError
 
-sealed interface GeneralRefreshError : AuthenticatedRefreshError
+sealed class NetworkError : AppError {
+    data object Connection : NetworkError()
+    data object Timeout : NetworkError()
+    data object Authentication : NetworkError()
+    data object ServerError : NetworkError()
+    data object Unknown : NetworkError()
+}
 
-data object AuthenticationError : AuthenticatedRefreshError
-data object ConnectionError : GeneralRefreshError
-data object ServerError : GeneralRefreshError
-data object TimeoutError : GeneralRefreshError
-data object StorageError : GeneralRefreshError
-data object UnknownError : GeneralRefreshError
+sealed class LocalError : AppError {
+    data object DiskFull : LocalError()
+    data object DatabaseCorrupt : LocalError()
+    data object DataStoreCorrupt : LocalError()
+    data object ConstraintViolation : LocalError()
+    data object IOError : LocalError()
+    data object Unknown : LocalError()
+}
